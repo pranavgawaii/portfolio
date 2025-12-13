6cac265a2c5b5e33ffb837cb3dd84ba66f4471e9
pranavgawaii <pranavvgawai@gmail.com>
2025-12-13 13:08:50 +0530

    Update ContactModal.tsx

--- components/ContactModal.tsx
+++ components/ContactModal.tsx
@@ -21,9 +21,9 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
 
     // REPLACE THESE WITH YOUR ACTUAL EMAILJS KEYS
     // Sign up at https://www.emailjs.com/
-    const SERVICE_ID = 'YOUR_SERVICE_ID';
-    const TEMPLATE_ID = 'YOUR_TEMPLATE_ID';
-    const PUBLIC_KEY = 'YOUR_PUBLIC_KEY';
+    const SERVICE_ID = 'service_tir69do';
+    const TEMPLATE_ID = 'template_h00tjmr';
+    const PUBLIC_KEY = 'CKf4MCbwmwQ3mA7UG';
 
     if (form.current) {
       emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
