901961263565f8f8a5fb8e9a7a6fd461f7b5e6e8
pranavgawaii <pranavvgawai@gmail.com>
2025-12-19 12:51:29 +0530

    Restore ThemeProvider

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globals.css';
-import { ThemeProvider } from './src/providers/ThemeProvider';
+import { ThemeProvider } from './components/ThemeProvider';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
